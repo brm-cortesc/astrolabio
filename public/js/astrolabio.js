d160a4d67860c47810cd2300cb9dcc736a14f3fa
Cristian Cortés <cristian.cortes@brm.com.co>
2016-07-07 12:25:28 -0500

    test sticky header

--- public/js/astrolabio.js
+++ public/js/astrolabio.js
@@ -5,4 +5,30 @@ $('.grid').isotope({
     // use outer width of grid-sizer for columnWidth
     columnWidth: '.grid-sizer'
   }
-})
+});
+
+/*sticky header*/
+jQuery(document).ready(function($) {
+	
+	$(window).scroll(function() {
+		if( $(window).scrollTop() >= 180 ){
+	
+			$('header')
+				.addClass('fixed')
+				.velocity({
+					backgroundColorAlpha: 0.5
+				},{
+					duration: 500,
+					easing: 'easeInOutSine'
+				});
+
+
+		}else{
+
+			$('header')
+				.removeClass('fixed')
+				.velocity('reverse');
+		}
+
+	});
+});
